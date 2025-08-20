8da62373ec3685ce54eefe34793f6bd89f11d695
Wooseong <thehousedone@gmail.com>
2025-08-20 18:02:15 +0900

    Initial Commit

---
+++ EventLogger/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  EventLogger
+//
+//  Created by 김우성 on 8/20/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+
+}
+
