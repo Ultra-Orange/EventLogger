2ea6b803da23102dbe739944a87d5f8bea372b5d
Wooseong <thehousedone@gmail.com>
2025-08-21 15:59:42 +0900

    add: #0-커스텀 폰트 추가

---
+++ EventLogger/Extensions/UIFont++.swift
@@ -0,0 +1,27 @@
+//
+//  UIFont++.swift
+//  EventLogger
+//
+//  Created by 김우성 on 8/21/25.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    // 타이틀 1, 2, 3
+    static let largeTitle = UIFont.systemFont(ofSize: 28, weight: .regular)
+    static let mediumTitle = UIFont.systemFont(ofSize: 22, weight: .regular)
+    static let smallTitle = UIFont.systemFont(ofSize: 20, weight: .regular)
+    
+    // 본문 등 용도
+    static let headline = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    static let body = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static let callout = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let subhead = UIFont.systemFont(ofSize: 15, weight: .regular)
+    static let footnote = UIFont.systemFont(ofSize: 13, weight: .regular)
+    
+}
+
+/// label.font = .title
+/// button.titleLabel?.font = .bodyBold
