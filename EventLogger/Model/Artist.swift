d1dcb725296763f9359ed573542a3df2f9cce6e3
Yoon-Mac <sryoowk@gmail.com>
2025-08-22 21:11:27 +0900

    feat: #4 아티스트 모델 추가

---
+++ EventLogger/Model/Artist.swift
@@ -0,0 +1,22 @@
+//
+//  Artist.swift
+//  EventLogger
+//
+//  Created by Yoon on 8/22/25.
+//
+
+import UIKit
+
+struct Artist: Hashable {
+    let id: UUID
+    let name: String
+    let color: UIColor?
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: Artist, rhs: Artist) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
