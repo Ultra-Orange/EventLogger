2b89dbbc2d6627f3ca736c1d22acde923fe7c85d
Yoon-Mac <sryoowk@gmail.com>
2025-08-25 11:27:02 +0900

    del: #6- 버튼을 사용하지 않기로 했기 때문에 삭제

--- EventLogger/Extensions/UIButton++Configuration.swift
+++ EventLogger/Extensions/UIButton++Configuration.swift
@@ -31,35 +31,5 @@ extension UIButton.Configuration {
         }
         return config
     }
-    
-    // 폼 필드용 공용 구성 (텍스트필드처럼 보이는 버튼)
-    static func formField(
-        title: String? = nil,
-        showsChevron: Bool = true
-    ) -> UIButton.Configuration {
-        var config: UIButton.Configuration = .plain()
-        
-        // 1) 콘텐츠 인셋 (상하10, 좌우16)
-        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16)
-
-        // 2) 제목
-        config.title = title
-        config.titleTextAttributesTransformer = .init { attr in
-            var newAttr = attr
-            newAttr.font = UIFont.preferredFont(forTextStyle: .footnote)
-            newAttr.foregroundColor = .secondaryLabel
-            return newAttr
-        }
-        
-        // 4) 외곽선(1px) + 모서리(10)
-        var bg = UIBackgroundConfiguration.clear()
-        bg.cornerRadius = 10
-        bg.strokeColor = .separator
-        bg.strokeWidth = 1.0 / UIScreen.main.scale   // 1px 보장
-        config.background = bg
-        
-        
-        return config
-    }
 
 }
