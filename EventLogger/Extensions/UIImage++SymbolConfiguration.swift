7eb108d0ae16027981b4976c23da299423ee8370
Yoon-Mac <sryoowk@gmail.com>
2025-08-25 16:07:24 +0900

    feat: config추가

--- EventLogger/Extensions/UIImage++SymbolConfiguration.swift
+++ EventLogger/Extensions/UIImage++SymbolConfiguration.swift
@@ -11,4 +11,5 @@ extension UIImage.Configuration {
     // SF Symbol Callout에 맞추기
     static let callout = UIImage.SymbolConfiguration(font: .preferredFont(forTextStyle: .callout))
     static let footnote = UIImage.SymbolConfiguration(font: .preferredFont(forTextStyle: .footnote))
+    static let addImageIcon = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 32))
 }
