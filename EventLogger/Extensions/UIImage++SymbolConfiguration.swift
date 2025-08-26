a0665bad795b02827c1c215f73eef8c289731f64
Yoon-Mac <sryoowk@gmail.com>
2025-08-26 15:00:55 +0900

    feat #13 : SF심볼 입력용 static 함수 추가

--- EventLogger/Extensions/UIImage++SymbolConfiguration.swift
+++ EventLogger/Extensions/UIImage++SymbolConfiguration.swift
@@ -12,3 +12,21 @@ extension UIImage.Configuration {
     static let font16Regular = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 16))
     static let font32Regular = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 32))
 }
+
+// 색이랑 함께 넣을 수 있는 static 함수
+extension UIImage {
+    static func SFSymbol(
+        named name: String,
+        config: UIImage.SymbolConfiguration,
+        color: UIColor = .white
+    ) -> UIImage? {
+        return UIImage(systemName: name)?
+            .withConfiguration(config)
+            .withTintColor(color, renderingMode: .alwaysOriginal)
+    }
+    
+    /*
+     사용 예제
+     private let mapPinIcon = UIImageView(image: UIImage.SFSymbol(named: "mappin.and.ellipse", config: .font16Regular))
+     */
+}
