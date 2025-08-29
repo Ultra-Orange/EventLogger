6a3d3962e928e31253c5695c98bb0d1637995ade
Wooseong <thehousedone@gmail.com>
2025-08-29 17:38:24 +0900

    fix: #20-정수, 실수 아닌 것 입력 막는 로직 제거

--- EventLogger/Scenes/Schedule/View/ExpenseFieldContainerView.swift
+++ EventLogger/Scenes/Schedule/View/ExpenseFieldContainerView.swift
@@ -10,6 +10,7 @@ import Then
 import UIKit
 
 final class ExpenseFieldContainerView: UIView {
+    
     let label = UILabel().then {
         $0.text = "비용"
         $0.font = .font13Regular
@@ -37,31 +38,11 @@ final class ExpenseFieldContainerView: UIView {
             $0.leading.trailing.equalToSuperview().inset(16)
         }
 
-        textField.delegate = self
-
         addSubview(textField)
         textField.snp.makeConstraints {
             $0.top.equalTo(label.snp.bottom).offset(8)
             $0.leading.bottom.trailing.equalToSuperview()
             $0.height.equalTo(42)
-        }
-    }
-}
-
-extension ExpenseFieldContainerView: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let currentText = textField.text ?? ""
-        guard let stringRange = Range(range, in: currentText) else { return false }
-        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-
-        if updatedText.isEmpty { return true } // 백스페이스 허용
-
-        if Int(updatedText) != nil {
-            return true // 정수
-        } else if Double(updatedText) != nil {
-            return true // 실수
-        } else {
-            return false // 숫자 아님
         }
     }
 }
