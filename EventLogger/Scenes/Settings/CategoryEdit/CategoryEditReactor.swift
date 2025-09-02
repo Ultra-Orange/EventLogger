a828316c0fb40e8acb5d5c6615cb5773e9985f5e
Yoon-Mac <sryoowk@gmail.com>
2025-09-02 16:51:41 +0900

    feat #43 카테고리 포지션 변경 & 삭제 구현

--- EventLogger/Scenes/Settings/CategoryEdit/CategoryEditReactor.swift
+++ EventLogger/Scenes/Settings/CategoryEdit/CategoryEditReactor.swift
@@ -15,13 +15,22 @@ import RxSwift
 
 final class CategoryEditReactor: BaseReactor {
     // 사용자 액션 정의 (사용자의 의도)
-    enum Action {}
+    enum Action {
+        case reloadCategories
+        case reorderCategories([CategoryItem])
+        case deleteCategory(CategoryItem)
+    }
 
     // 상태변경 이벤트 정의 (상태를 어떻게 바꿀 것인가)
-    enum Mutation {}
+    enum Mutation {
+        case setCategories([CategoryItem])
+        case setAlertMessage(String)
+    }
 
     // View의 상태 정의 (현재 View의 상태값)
     struct State {
+        var categories: [CategoryItem] = []
+        @Pulse var alertMessage: String?
     }
 
     // 생성자에서 초기 상태 설정
@@ -34,14 +43,39 @@ final class CategoryEditReactor: BaseReactor {
     // Action이 들어왔을 때 어떤 Mutation으로 바뀔지 정의
     // 사용자 입력 → 상태 변화 신호로 변환
     func mutate(action: Action) -> Observable<Mutation> {
-        switch action {}
+        @Dependency(\.swiftDataManager) var swiftDataManager
+        switch action {
+        case .reloadCategories:
+            let categories = swiftDataManager.fetchAllCategories()
+            return .just(.setCategories(categories))
+        case let .reorderCategories(items):
+            swiftDataManager.updateCategoriesPosition(items)
+            return .just(.setCategories(items))
+        case let .deleteCategory(item):
+            let categories = currentState.categories.filter { $0.id != item.id }
+            do {
+                try swiftDataManager.deleteCategory(id: item.id)
+                return .just(.setCategories(categories))
+            } catch SwiftDataMangerError.cannotDeleteUsedCategory {
+                return .just(.setAlertMessage("일정에서 사용하고 있는 \n 카테고리는 삭제할 수 없습니다."))
+            } catch SwiftDataMangerError.cannotDeleteLastCategory {
+                return .just(.setAlertMessage("최소 1개의 카테고리는 존재해야 합니다."))
+            } catch {
+                return .just(.setAlertMessage("알 수 없는 오류가 발생했습니다."))
+            }
+        }
     }
 
     // Mutation이 발생했을 때 상태(State)를 실제로 바꿈
     // 상태 변화 신호 → 실제 상태 반영
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-        switch mutation {}
+        switch mutation {
+        case let .setCategories(categories):
+            newState.categories = categories
+        case let .setAlertMessage(message):
+            newState.alertMessage = message
+        }
         return newState
     }
 }
--- EventLogger/Scenes/Settings/CategoryEdit/CategoryEditViewController.swift
+++ EventLogger/Scenes/Settings/CategoryEdit/CategoryEditViewController.swift
@@ -16,10 +16,6 @@ import Then
 class CategoryEditViewController: BaseViewController<CategoryEditReactor> {
     // MARK: UI Component
     
-    let navEditButton = UIButton(configuration: .navEdit).then{
-        $0.configuration?.title = "편집"
-    }
-    
     let addButton = UIButton(configuration: .bottomButton).then{
         $0.configuration?.title = "추가하기"
     }
@@ -29,18 +25,31 @@ class CategoryEditViewController: BaseViewController<CategoryEditReactor> {
         $0.textColor = .label
     }
     
+    // CollectionView
+    
+    let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout.list(using: UICollectionLayoutListConfiguration(appearance: .insetGrouped))
+    )
+    
+    lazy var dataSource = makeDataSource(collectionView)
+    
+    let reorderCategoryRelay = PublishRelay<[CategoryItem]>()
+    let deleteCategoryRelay = PublishRelay<CategoryItem>()
+    
     // MARK: SetupUI
-
+    
     override func setupUI() {
         view.backgroundColor = .systemBackground
         title = "카테고리 목록"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: navEditButton)
+        navigationItem.rightBarButtonItem = editButtonItem
         
-        view.addSubview(tmpLabel)
+        view.addSubview(collectionView)
         view.addSubview(addButton)
         
-        tmpLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        collectionView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(addButton.snp.top)
         }
         
         addButton.snp.makeConstraints {
@@ -50,11 +59,102 @@ class CategoryEditViewController: BaseViewController<CategoryEditReactor> {
         }
         
     }
-
+    
     // MARK: Binding
-
+    
     override func bind(reactor: CategoryEditReactor) {
+        reactor.state.map { $0.categories }
+            .bind { [weak self] items in
+                guard let self else { return }
+                applySnapshot(items)
+            }
+            .disposed(by: disposeBag)
         
-                
+        reorderCategoryRelay.map { items in .reorderCategories(items) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        deleteCategoryRelay
+            .withUnretained(self)
+            .flatMap { `self`, item in
+                UIAlertController.rx.alert(on: self, title: "카테고리 삭제", message: "정말로 이 카테고리 삭제하시겠습니까?", actions: [
+                    .cancel("취소"),
+                    .destructive("삭제", payload: item),
+                ])
+            }
+            .map { item in .deleteCategory(item) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.pulse(\.$alertMessage)
+            .withUnretained(self)
+            .flatMap { `self`, message in
+                UIAlertController.rx.alert(on: self, title: "삭제 실패", message: message, actions: [
+                    .action("확인", payload: ())
+                ])
+            }
+            .bind{}
+            .disposed(by: disposeBag)
+        
+        
+        rx.viewWillAppear.map { _ in .reloadCategories }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        addButton.rx.tap
+            .bind {
+                print("tap")
+            }
+            .disposed(by: disposeBag)
+        
     }
+    
+    
+}
+
+extension CategoryEditViewController {
+    
+    // 편집 모드
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        collectionView.isEditing = editing
+    }
+    
+    private func makeDataSource(_ collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Int, CategoryItem> {
+        let deleteRelay = deleteCategoryRelay
+        let cellRegistration = UICollectionView.CellRegistration<CategoryEditCell, CategoryItem> { cell, indexPath, item in
+            cell.configureCell(item: item)
+            cell.accessories = [
+                .disclosureIndicator(displayed: .whenNotEditing, options: UICellAccessory.DisclosureIndicatorOptions(tintColor: .neutral50)),
+                .reorder(displayed: .whenEditing, options: UICellAccessory.ReorderOptions(tintColor: .neutral50)),
+                .delete(displayed: .whenEditing, options: UICellAccessory.DeleteOptions(tintColor:.neutral50, backgroundColor: .systemRed)) {
+                    deleteRelay.accept(item)
+                }
+            ]
+        }
+        
+        var reorderingHandlers = UICollectionViewDiffableDataSource<Int, CategoryItem>.ReorderingHandlers()
+        reorderingHandlers.canReorderItem = { _ in true }
+        reorderingHandlers.didReorder = { [weak self] transaction in
+            guard let self else { return }
+            let updatedItems = self.dataSource.snapshot(for: 0).items
+            reorderCategoryRelay.accept(updatedItems)
+        }
+        
+        let dataSource = UICollectionViewDiffableDataSource<Int, CategoryItem>(collectionView: collectionView) { collectionView, indexPath, text in
+            collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: text)
+        }
+        dataSource.reorderingHandlers = reorderingHandlers
+        
+        return dataSource
+    }
+    
+    
+    func applySnapshot(_ items: [CategoryItem]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, CategoryItem>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(items, toSection: 0)
+        dataSource.apply(snapshot)
+    }
+    
 }
